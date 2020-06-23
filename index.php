24c73b418ba3f248a4f429c8bc217e3d84d713b9
TinhTV <Tinhtv1910@gmail.com>
2020-06-23 15:14:25 +0700

    ASM

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="" href="min.js">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</head>
+<body>
+	<form action="electro/index.html" method="POST">
+		<div class="container">
+			<div class="left-section">
+				<div class="header">
+					<h1 class="animation a1">Welcome Back!</h1>
+					<h4 class="animation a2">Log in to see and select products.</h4>
+				</div>
+				<div class="form">
+					<input type="email" class="form-field animation a3" placeholder="Username">
+					<input type="password" class="form-field animation a4" placeholder="Password">
+					<p class="animation a5"><a href="#">Forgot Password</a></p>
+					<button class="animation a6">LOGIN</button>
+				</div>
+			</div>
+			<div class="right-section"></div>
+		</div>
+	</form>
+</body>
+</html>
